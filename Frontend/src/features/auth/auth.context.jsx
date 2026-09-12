0f1fff205c519dd16a99f0c72c10a8289352931d
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-09-12 22:06:53 +0530

    fixing loading stage

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,12 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { getMe } from "./services/auth.api";
 
 export const AuthContext = createContext()
-
 
 export const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function checkAuth() {
+            try {
+                const data = await getMe();
+                setUser(data.user);
+            } catch (err) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        }
+        checkAuth();
+    }, []);
 
     return (
         <AuthContext.Provider value={{ user, setUser, loading, setLoading }}>
